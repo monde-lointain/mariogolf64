a0fa2d33f0ed826818ef03245bb00e9dc7ddebbe
Clyde Williams <hunterw297@gmail.com>
2026-01-21 16:22:51 -0600

    Add ELF section header parsing to overlay script

--- ghidra/create_overlays.py
+++ ghidra/create_overlays.py
@@ -40,9 +40,59 @@ def parse_elf_header(data):
 
     return (e_shoff, e_shnum, e_shstrndx, e_shentsize)
 
+def parse_section_header(data, offset):
+    """Parse single section header at offset.
+    Returns dict with: name_idx, type, flags, addr, offset, size."""
+    fields = struct.unpack('>IIIIIIIIII', data[offset:offset+40])
+    return {
+        'name_idx': fields[0],   # sh_name - index into string table
+        'type': fields[1],       # sh_type
+        'flags': fields[2],      # sh_flags
+        'addr': fields[3],       # sh_addr (VMA)
+        'offset': fields[4],     # sh_offset (file offset)
+        'size': fields[5],       # sh_size
+    }
+
+def get_section_name(data, strtab_offset, name_idx):
+    """Get null-terminated string from string table."""
+    start = strtab_offset + name_idx
+    end = data.index(b'\x00', start)
+    return data[start:end].decode('ascii')
+
+def parse_all_sections(data):
+    """Parse all section headers, return list of dicts with name resolved."""
+    e_shoff, e_shnum, e_shstrndx, e_shentsize = parse_elf_header(data)
+
+    # First, get string table section to resolve names
+    strtab_hdr = parse_section_header(data, e_shoff + e_shstrndx * e_shentsize)
+    strtab_offset = strtab_hdr['offset']
+
+    sections = []
+    for i in range(e_shnum):
+        offset = e_shoff + i * e_shentsize
+        hdr = parse_section_header(data, offset)
+        hdr['name'] = get_section_name(data, strtab_offset, hdr['name_idx'])
+        sections.append(hdr)
+
+    return sections
+
 def run():
-    # Placeholder - will be expanded
-    println("Ghidra Overlay Creator - Skeleton")
+    elf_path = askFile("Select ELF file", "Open")
+    if elf_path is None:
+        println("Cancelled")
+        return
+
+    println("Reading ELF: " + str(elf_path))
+    data = read_file(str(elf_path))
+
+    sections = parse_all_sections(data)
+    println("Found %d sections" % len(sections))
+
+    # Print overlay sections
+    for sec in sections:
+        if sec['name'].startswith('.overlay'):
+            println("  %s: addr=0x%08X size=0x%X offset=0x%X" % (
+                sec['name'], sec['addr'], sec['size'], sec['offset']))
 
 if __name__ == "__main__":
     run()
