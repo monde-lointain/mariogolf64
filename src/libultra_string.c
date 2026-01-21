7f48b9e4ea91f712d4af20f1d61d6c201173cac1
Clyde Williams <hunterw297@gmail.com>
2026-01-21 09:20:47 -0600

    Add libultra_string.c with strchr stub

---
+++ src/libultra_string.c
@@ -0,0 +1,7 @@
+#include "common.h"
+
+INCLUDE_ASM("asm/nonmatchings/libultra_string", strchr);
+
+INCLUDE_ASM("asm/nonmatchings/libultra_string", func_800AADA0);
+
+INCLUDE_ASM("asm/nonmatchings/libultra_string", func_800AADC4);
