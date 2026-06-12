058eeec88de3dd65484140175aa763f06e879763
Clyde Williams <hunterw297@gmail.com>
2026-06-11 20:19:55 -0500

    Bank osViSetEvent as libultra/monegi/vi/visetevent.c (upstream mirror)
    vi-band sibling pair (2/2). Verbatim copy of libultra_modern visetevent.c;
    same __osViNext + headers as viswapbuf/visetmode, zero enablers. Dead _DEBUG
    return-0 block compiles out (matches visetmode.c). 0 INCLUDE_ASM, make ROM
    SHA-1 == baserom.
    
    md5-candidate 13->14; matched 15->16/2090 (~0.77%).

--- src/libultra/monegi/vi/visetevent.c
+++ src/libultra/monegi/vi/visetevent.c
@@ -1,3 +1,24 @@
-#include "common.h"
+#include "PR/os_internal.h"
+#include "PR/ultraerror.h"
+#include "assert.h"
+#include "viint.h"
 
-INCLUDE_ASM("asm/nonmatchings/libultra/monegi/vi/visetevent", osViSetEvent);
+void osViSetEvent(OSMesgQueue* mq, OSMesg m, u32 retraceCount) {
+    register u32 saveMask;
+
+#ifdef _DEBUG
+    if (!__osViDevMgr.active) {
+        __osError(ERR_OSVISETEVENT, 0);
+        return 0;
+    }
+
+    assert(mq != NULL);
+#endif
+
+    saveMask = __osDisableInt();
+
+    __osViNext->msgq = mq;
+    __osViNext->msg = m;
+    __osViNext->retraceCount = retraceCount;
+    __osRestoreInt(saveMask);
+}
