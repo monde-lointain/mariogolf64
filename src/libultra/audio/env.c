01d801be5daaff8a16f9b5ef8b1e4ab856cb460d
Clyde Williams <hunterw297@gmail.com>
2026-06-16 08:25:10 -0500

    S101: split env|filter subseg, bank libultra audio/filter.c (alFilterNew verbatim cp)
    Gate: split [0x804D0,asm] -> [0x804D0,c,env]+[0x81180,c,filter]; symbol_addrs += env members
    (_frexpf/_ldexpf/_getRate/_getVol; alFilterNew already present). filter.c verbatim ultralib J,
    0 edits, ROM SHA-1 == baserom. env.c scaffold (7 stubs) banked next.

---
+++ src/libultra/audio/env.c
@@ -0,0 +1,15 @@
+#include "common.h"
+
+INCLUDE_ASM("asm/nonmatchings/libultra/audio/env", alEnvmixerPull);
+
+INCLUDE_ASM("asm/nonmatchings/libultra/audio/env", alEnvmixerParam);
+
+INCLUDE_ASM("asm/nonmatchings/libultra/audio/env", _pullSubFrame);
+
+INCLUDE_ASM("asm/nonmatchings/libultra/audio/env", _frexpf);
+
+INCLUDE_ASM("asm/nonmatchings/libultra/audio/env", _ldexpf);
+
+INCLUDE_ASM("asm/nonmatchings/libultra/audio/env", _getRate);
+
+INCLUDE_ASM("asm/nonmatchings/libultra/audio/env", _getVol);
