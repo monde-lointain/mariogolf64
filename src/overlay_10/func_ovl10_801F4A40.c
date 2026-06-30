d73b051200889ad234698762f33a1636bc25fddc
Clyde Williams <hunterw297@gmail.com>
2026-06-30 16:37:56 -0500

    S148 freeze: overlay_10/func_ovl10_801F4A40 pack, seed 13 classical

---
+++ src/overlay_10/func_ovl10_801F4A40.c
@@ -0,0 +1,5 @@
+#include "common.h"
+
+INCLUDE_ASM("asm/nonmatchings/overlay_10/func_ovl10_801F4A40", func_ovl10_801F4A40);
+
+INCLUDE_ASM("asm/nonmatchings/overlay_10/func_ovl10_801F4A40", func_ovl10_801F4AD8);
