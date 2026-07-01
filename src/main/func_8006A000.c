b9e7da4165b5cbe59237ceaea2edfec8e475930e
Clyde Williams <hunterw297@gmail.com>
2026-07-01 10:15:46 -0500

    S152 freeze: main/func_8006A000 pack (3-fn decomposed FP vector-math slice), seed 13 classical

---
+++ src/main/func_8006A000.c
@@ -0,0 +1,7 @@
+#include "common.h"
+
+INCLUDE_ASM("asm/nonmatchings/main/func_8006A000", vector_magnitude_safe);
+
+INCLUDE_ASM("asm/nonmatchings/main/func_8006A000", calculate_hypotenuse_safe);
+
+INCLUDE_ASM("asm/nonmatchings/main/func_8006A000", func_8006A174);
