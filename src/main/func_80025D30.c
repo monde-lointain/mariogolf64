e08d0d3a847e585e1bd41971170ef4789c06d745
Clyde Williams <hunterw297@gmail.com>
2026-07-01 15:03:48 -0500

    S157 freeze: main/func_80025D30 9-fn integer overlay-loader (whole subseg [0x1130], 8-gate PO-approved run-whole), seed 13 classical

---
+++ src/main/func_80025D30.c
@@ -0,0 +1,19 @@
+#include "common.h"
+
+INCLUDE_ASM("asm/nonmatchings/main/func_80025D30", func_80025D30);
+
+INCLUDE_ASM("asm/nonmatchings/main/func_80025D30", func_80025D54);
+
+INCLUDE_ASM("asm/nonmatchings/main/func_80025D30", func_80025D78);
+
+INCLUDE_ASM("asm/nonmatchings/main/func_80025D30", func_80025D8C);
+
+INCLUDE_ASM("asm/nonmatchings/main/func_80025D30", func_80025EC8);
+
+INCLUDE_ASM("asm/nonmatchings/main/func_80025D30", func_80025F18);
+
+INCLUDE_ASM("asm/nonmatchings/main/func_80025D30", load_overlay);
+
+INCLUDE_ASM("asm/nonmatchings/main/func_80025D30", unload_overlay);
+
+INCLUDE_ASM("asm/nonmatchings/main/func_80025D30", func_80026358);
