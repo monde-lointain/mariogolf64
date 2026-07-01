8a1450a6c5b3b56c48f128a6f6f684f234b85f3c
Clyde Williams <hunterw297@gmail.com>
2026-07-01 11:37:01 -0500

    S154 freeze: main/func_8006A1C0 3-fn tail (irreducible remainder of func_8006A000 one-tu, 3+2+3 decomp), seed 13 classical

---
+++ src/main/func_8006A1C0.c
@@ -0,0 +1,7 @@
+#include "common.h"
+
+INCLUDE_ASM("asm/nonmatchings/main/func_8006A1C0", calc_vec3_magnitude);
+
+INCLUDE_ASM("asm/nonmatchings/main/func_8006A1C0", func_8006A1EC);
+
+INCLUDE_ASM("asm/nonmatchings/main/func_8006A1C0", func_8006A274);
