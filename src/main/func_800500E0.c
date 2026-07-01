69d3e11b7a9f753ce9586b57451a818573d0b3c3
Clyde Williams <hunterw297@gmail.com>
2026-07-01 08:39:55 -0500

    S151 freeze: main/func_800500E0 pack (3fn Gfx DL builder), seed 13 classical

---
+++ src/main/func_800500E0.c
@@ -0,0 +1,7 @@
+#include "common.h"
+
+INCLUDE_ASM("asm/nonmatchings/main/func_800500E0", func_800500E0);
+
+INCLUDE_ASM("asm/nonmatchings/main/func_800500E0", func_80050274);
+
+INCLUDE_ASM("asm/nonmatchings/main/func_800500E0", func_8005029C);
