f97ad3c8add345598200540396e3ce3fad9d7efd
Clyde Williams <hunterw297@gmail.com>
2026-07-01 17:20:36 -0500

    S158 freeze: main/func_80067D40 5-fn one-tu classical slice ([0x43140] whole subseg, 1744B, rodata-straddle 0x800D1370, 8-gate one-tu exemption run-whole), seed 13 classical

---
+++ src/main/func_80067D40.c
@@ -0,0 +1,11 @@
+#include "common.h"
+
+INCLUDE_ASM("asm/nonmatchings/main/func_80067D40", func_80067D40);
+
+INCLUDE_ASM("asm/nonmatchings/main/func_80067D40", func_80067D4C);
+
+INCLUDE_ASM("asm/nonmatchings/main/func_80067D40", func_80067D74);
+
+INCLUDE_ASM("asm/nonmatchings/main/func_80067D40", func_800680FC);
+
+INCLUDE_ASM("asm/nonmatchings/main/func_80067D40", func_80068308);
