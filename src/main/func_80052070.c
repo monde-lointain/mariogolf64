f5d0820b7d1a9271040bcc7d5f654ee2d4fa27c8
Clyde Williams <hunterw297@gmail.com>
2026-07-01 13:45:51 -0500

    S156 freeze: main/func_80052070 4-fn jal-free accessor slice (decomposed off func_80051E90 jtbl pair), seed 5 classical

---
+++ src/main/func_80052070.c
@@ -0,0 +1,9 @@
+#include "common.h"
+
+INCLUDE_ASM("asm/nonmatchings/main/func_80052070", func_80052070);
+
+INCLUDE_ASM("asm/nonmatchings/main/func_80052070", func_800520DC);
+
+INCLUDE_ASM("asm/nonmatchings/main/func_80052070", func_80052100);
+
+INCLUDE_ASM("asm/nonmatchings/main/func_80052070", func_80052168);
