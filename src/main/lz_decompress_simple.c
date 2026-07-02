a0c50b136c50874421cff54ead2923205850eb2d
Clyde Williams <hunterw297@gmail.com>
2026-07-02 17:32:45 -0500

    S164 freeze: main/lz_decompress_simple 3-fn LZ-decompress trio (split [0x43810] head slice [0x43810..0x440A0], 2192B, 2 pure-integer 0-jal leaves + 1 OS/DMA wrapper w/ internal jal, no rodata/data carve, no symbol adds, 8-gate CLEAR), seed 5 classical

---
+++ src/main/lz_decompress_simple.c
@@ -0,0 +1,7 @@
+#include "common.h"
+
+INCLUDE_ASM("asm/nonmatchings/main/lz_decompress_simple", lz_decompress_simple);
+
+INCLUDE_ASM("asm/nonmatchings/main/lz_decompress_simple", lz_decompress_extended);
+
+INCLUDE_ASM("asm/nonmatchings/main/lz_decompress_simple", lz_decompress_dma);
