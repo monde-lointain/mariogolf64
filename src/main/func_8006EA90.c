477084d59585f02302e82e6e9ef0b82e3ead2a5d
Clyde Williams <hunterw297@gmail.com>
2026-07-02 00:10:08 -0500

    S160 freeze: main/func_8006EA90 3-fn one-tu classical slice ([0x49E90] whole subseg, 1808B, FP-double rodata-straddle D_800D1490..14B8, DL builder func_8006ED34 #display-lists, one-tu exemption run-whole), seed 13 classical

---
+++ src/main/func_8006EA90.c
@@ -0,0 +1,8 @@
+#include "common.h"
+
+INCLUDE_ASM("asm/nonmatchings/main/func_8006EA90", func_8006EA90);
+
+void func_8006ED2C(void) {
+}
+
+INCLUDE_ASM("asm/nonmatchings/main/func_8006EA90", func_8006ED34);
