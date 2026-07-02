f0aafd3694000182ae8903026e0b120426e050d0
Clyde Williams <hunterw297@gmail.com>
2026-07-02 13:59:40 -0500

    S162 freeze: main/func_80076500 3-fn debug-Vec3f-buffer head (split [0x51900] head slice [0x51900..0x51A40], 320B, 1-jal osSyncPrintf, extern-string D_800D1850, no rodata carve, 8-gate CLEAR), seed 5 classical

---
+++ src/main/func_80076500.c
@@ -0,0 +1,7 @@
+#include "common.h"
+
+INCLUDE_ASM("asm/nonmatchings/main/func_80076500", func_80076500);
+
+INCLUDE_ASM("asm/nonmatchings/main/func_80076500", func_8007654C);
+
+INCLUDE_ASM("asm/nonmatchings/main/func_80076500", func_80076558);
