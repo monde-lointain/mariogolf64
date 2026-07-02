b41b0873af65b07f41e854218b0ea3ba1003fe16
Clyde Williams <hunterw297@gmail.com>
2026-07-02 08:16:07 -0500

    S161 freeze: main/func_800521C0 3-fn scenario-flag head (split [0x2D5C0] head slice [0x2D5C0..0x2D650], 144B, no rodata, jal-light, 8-gate CLEAR), seed 3 classical

---
+++ src/main/func_800521C0.c
@@ -0,0 +1,7 @@
+#include "common.h"
+
+INCLUDE_ASM("asm/nonmatchings/main/func_800521C0", func_800521C0);
+
+INCLUDE_ASM("asm/nonmatchings/main/func_800521C0", func_800521DC);
+
+INCLUDE_ASM("asm/nonmatchings/main/func_800521C0", func_80052220);
