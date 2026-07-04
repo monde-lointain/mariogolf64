24939afced4a22c36962d6b471c77013fc78126d
Clyde Williams <hunterw297@gmail.com>
2026-07-03 23:14:58 -0500

    S170 freeze: func_8004DD70.c 3-fn slot module, seed 3 classical

---
+++ src/main/func_8004DD70.c
@@ -0,0 +1,7 @@
+#include "common.h"
+
+INCLUDE_ASM("asm/nonmatchings/main/func_8004DD70", func_8004DD70);
+
+INCLUDE_ASM("asm/nonmatchings/main/func_8004DD70", func_8004DDE4);
+
+INCLUDE_ASM("asm/nonmatchings/main/func_8004DD70", func_8004DE44);
