24d03a55a6f69a0b775a716d13ea90ea2a379257
Clyde Williams <hunterw297@gmail.com>
2026-07-04 00:04:13 -0500

    S171 freeze: print_string_at_grid.c 6-fn grid-print cluster, seed 5 classical

---
+++ src/main/print_string_at_grid.c
@@ -0,0 +1,13 @@
+#include "common.h"
+
+INCLUDE_ASM("asm/nonmatchings/main/print_string_at_grid", print_string_at_grid);
+
+INCLUDE_ASM("asm/nonmatchings/main/print_string_at_grid", check_and_print_grid);
+
+INCLUDE_ASM("asm/nonmatchings/main/print_string_at_grid", func_8004DA4C);
+
+INCLUDE_ASM("asm/nonmatchings/main/print_string_at_grid", convert_and_print_hex);
+
+INCLUDE_ASM("asm/nonmatchings/main/print_string_at_grid", func_8004DAF4);
+
+INCLUDE_ASM("asm/nonmatchings/main/print_string_at_grid", func_8004DC44);
