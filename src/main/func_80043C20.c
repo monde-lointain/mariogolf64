f0eae20869696ee4d501cc8c604b3ffeeef318f0
Clyde Williams <hunterw297@gmail.com>
2026-07-05 09:31:53 -0500

    S184: bank 7 trivial getters/wrappers in func_80043C20.c (get_club_distance_slot/param/shot_data/shot_param/shot_progress, func_80044A8C/44C7C); ClubShot 0x34 struct; get_club_param single-exit if/else [7/21]

--- src/main/func_80043C20.c
+++ src/main/func_80043C20.c
@@ -1,5 +1,36 @@
 #include "common.h"
 
+typedef struct {
+  u8 pad00[0x18];
+  s32 dist[3];  // 0x18, 0x1C, 0x20
+  u8 pad24[4];  // 0x24
+  s16 unk28;    // 0x28
+  s16 unk2A;    // 0x2A
+  s16 unk2C;    // 0x2C
+  u8 pad2E[2];  // 0x2E
+  s8 unk30;     // 0x30
+  u8 pad31[3];  // 0x31
+} ClubShot;     // 0x34
+
+extern ClubShot D_800BB258[];
+extern ClubShot club_param_fallback;
+extern s16 D_801B7222[][92];
+extern s32 active_player_idx;
+extern s32 active_club_id;
+extern s32 active_meter_category;
+extern void* D_800FBE70;
+
+extern s32 flag_is_set(s32 flag);
+extern s32 get_club_meter_extent(void* club, s32 category);
+extern s32 get_club_meter_units(void* club, s32 category);
+extern void* get_table_entry(void* table);
+extern u8* func_80043AF0(s32 index);
+
+s32 resolve_club_terrain_mask(s32 arg0, void* arg1);
+s32 resolve_shot_quality_table(s32 arg0, s32 arg1, s32 arg2);
+void predict_shot_distance(s32 index);
+ClubShot* get_shot_data(void);
+
 INCLUDE_ASM("asm/nonmatchings/main/func_80043C20", func_80043C20);
 
 INCLUDE_ASM("asm/nonmatchings/main/func_80043C20", func_80043C64);
@@ -18,23 +49,48 @@ INCLUDE_ASM("asm/nonmatchings/main/func_80043C20", lerp_int);
 
 INCLUDE_ASM("asm/nonmatchings/main/func_80043C20", predict_shot_distance_variant);
 
-INCLUDE_ASM("asm/nonmatchings/main/func_80043C20", get_club_distance_slot);
+s32 get_club_distance_slot(ClubShot* c, s32 i) { return c->dist[i]; }
 
-INCLUDE_ASM("asm/nonmatchings/main/func_80043C20", get_club_param);
+ClubShot* get_club_param(u32 id) {
+  ClubShot* result;
+  if (id < 0xFC) {
+    result = &D_800BB258[id];
+  } else {
+    result = &club_param_fallback;
+  }
+  return result;
+}
 
-INCLUDE_ASM("asm/nonmatchings/main/func_80043C20", get_shot_data);
+ClubShot* get_shot_data(void) {
+  return &D_800BB258[D_801B7222[active_player_idx][active_club_id]];
+}
 
-INCLUDE_ASM("asm/nonmatchings/main/func_80043C20", get_shot_param);
+s32 get_shot_param(void) {
+  return get_club_meter_extent(get_shot_data(), active_meter_category);
+}
 
-INCLUDE_ASM("asm/nonmatchings/main/func_80043C20", get_shot_progress);
+s32 get_shot_progress(void) {
+  return get_club_meter_units(get_shot_data(), active_meter_category);
+}
 
 INCLUDE_ASM("asm/nonmatchings/main/func_80043C20", resolve_club_terrain_mask);
 
-INCLUDE_ASM("asm/nonmatchings/main/func_80043C20", func_80044A8C);
+s32 func_80044A8C(s32 arg0) {
+  return resolve_club_terrain_mask(arg0, get_table_entry(D_800FBE70));
+}
 
 INCLUDE_ASM("asm/nonmatchings/main/func_80043C20", resolve_shot_quality_table);
 
-INCLUDE_ASM("asm/nonmatchings/main/func_80043C20", func_80044C7C);
+void func_80044C7C(s32 arg0, s32 arg1, s32* out, s32 arg3) {
+  s32 n = resolve_shot_quality_table(arg0, arg1, arg3);
+  if (n > 0) {
+    out[0] = n * 2 - 1;
+    out[1] = n - 1;
+  } else {
+    out[0] = 0;
+    out[1] = 0;
+  }
+}
 
 INCLUDE_ASM("asm/nonmatchings/main/func_80043C20", func_80044CCC);
 
