f3ceaae7563a4d596f8a6a3bd86eec2e8a68807c
Clyde Williams <hunterw297@gmail.com>
2026-07-05 01:57:15 -0500

    S182: bank clear_text_grid + set_flag_based_on_param (func_8004D190.c 2/7)

--- src/main/func_8004D190.c
+++ src/main/func_8004D190.c
@@ -1,5 +1,8 @@
 #include "common.h"
 
+extern s8 flag;
+extern u8 D_800DAF60[];
+
 INCLUDE_ASM("asm/nonmatchings/main/func_8004D190", func_8004D190);
 
 INCLUDE_ASM("asm/nonmatchings/main/func_8004D190", func_8004D4B8);
@@ -8,8 +11,21 @@ INCLUDE_ASM("asm/nonmatchings/main/func_8004D190", func_8004D580);
 
 INCLUDE_ASM("asm/nonmatchings/main/func_8004D190", func_8004D5F0);
 
-INCLUDE_ASM("asm/nonmatchings/main/func_8004D190", func_8004D794);
+void clear_text_grid(void) {
+  s32 i = 0x4AF;
+  u8* p = &D_800DAF60[0x4AF];
+
+  for (; i >= 0; i--) {
+    *p-- = 0;
+  }
+}
 
 INCLUDE_ASM("asm/nonmatchings/main/func_8004D190", func_8004D7B8);
 
-INCLUDE_ASM("asm/nonmatchings/main/func_8004D190", set_flag_based_on_param);
+void set_flag_based_on_param(s32 enable) {
+  if (enable) {
+    flag = -0x80;
+  } else {
+    flag = 0;
+  }
+}
