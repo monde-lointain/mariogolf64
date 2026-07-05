50b3f427bb5ea171e5df003fc06cdbde3e36d11e
Clyde Williams <hunterw297@gmail.com>
2026-07-05 09:16:40 -0500

    S184 freeze: func_80043C20.c 21fn main-segment [0x1F020] golf-shot/club logic pack, seed 13 classical

---
+++ src/main/func_80043C20.c
@@ -0,0 +1,43 @@
+#include "common.h"
+
+INCLUDE_ASM("asm/nonmatchings/main/func_80043C20", func_80043C20);
+
+INCLUDE_ASM("asm/nonmatchings/main/func_80043C20", func_80043C64);
+
+INCLUDE_ASM("asm/nonmatchings/main/func_80043C20", lerp_int_v2);
+
+INCLUDE_ASM("asm/nonmatchings/main/func_80043C20", predict_shot_distance);
+
+INCLUDE_ASM("asm/nonmatchings/main/func_80043C20", func_80044254);
+
+INCLUDE_ASM("asm/nonmatchings/main/func_80043C20", func_80044470);
+
+INCLUDE_ASM("asm/nonmatchings/main/func_80043C20", func_800444B8);
+
+INCLUDE_ASM("asm/nonmatchings/main/func_80043C20", lerp_int);
+
+INCLUDE_ASM("asm/nonmatchings/main/func_80043C20", predict_shot_distance_variant);
+
+INCLUDE_ASM("asm/nonmatchings/main/func_80043C20", get_club_distance_slot);
+
+INCLUDE_ASM("asm/nonmatchings/main/func_80043C20", get_club_param);
+
+INCLUDE_ASM("asm/nonmatchings/main/func_80043C20", get_shot_data);
+
+INCLUDE_ASM("asm/nonmatchings/main/func_80043C20", get_shot_param);
+
+INCLUDE_ASM("asm/nonmatchings/main/func_80043C20", get_shot_progress);
+
+INCLUDE_ASM("asm/nonmatchings/main/func_80043C20", resolve_club_terrain_mask);
+
+INCLUDE_ASM("asm/nonmatchings/main/func_80043C20", func_80044A8C);
+
+INCLUDE_ASM("asm/nonmatchings/main/func_80043C20", resolve_shot_quality_table);
+
+INCLUDE_ASM("asm/nonmatchings/main/func_80043C20", func_80044C7C);
+
+INCLUDE_ASM("asm/nonmatchings/main/func_80043C20", func_80044CCC);
+
+INCLUDE_ASM("asm/nonmatchings/main/func_80043C20", func_80044FDC);
+
+INCLUDE_ASM("asm/nonmatchings/main/func_80043C20", func_800451E4);
