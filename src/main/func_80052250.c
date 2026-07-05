916a04f29e38402f871f8776f05c1ee98a142917
Clyde Williams <hunterw297@gmail.com>
2026-07-05 07:45:57 -0500

    S183 freeze: func_80052250.c 9fn main-segment [0x2D650] game-code pack, seed 13 classical

---
+++ src/main/func_80052250.c
@@ -0,0 +1,19 @@
+#include "common.h"
+
+INCLUDE_ASM("asm/nonmatchings/main/func_80052250", func_80052250);
+
+INCLUDE_ASM("asm/nonmatchings/main/func_80052250", func_80052264);
+
+INCLUDE_ASM("asm/nonmatchings/main/func_80052250", func_80052324);
+
+INCLUDE_ASM("asm/nonmatchings/main/func_80052250", func_80052384);
+
+INCLUDE_ASM("asm/nonmatchings/main/func_80052250", func_8005244C);
+
+INCLUDE_ASM("asm/nonmatchings/main/func_80052250", func_800525C4);
+
+INCLUDE_ASM("asm/nonmatchings/main/func_80052250", func_80052834);
+
+INCLUDE_ASM("asm/nonmatchings/main/func_80052250", func_80052A68);
+
+INCLUDE_ASM("asm/nonmatchings/main/func_80052250", func_80052CF0);
