7549e1a7d6123307ec4e20ea8c59f2ad84652927
Clyde Williams <hunterw297@gmail.com>
2026-07-05 01:50:17 -0500

    S182 freeze: func_8004D190.c 7fn VI/framebuffer+grid-print pack, seed 13 classical

---
+++ src/main/func_8004D190.c
@@ -0,0 +1,15 @@
+#include "common.h"
+
+INCLUDE_ASM("asm/nonmatchings/main/func_8004D190", func_8004D190);
+
+INCLUDE_ASM("asm/nonmatchings/main/func_8004D190", func_8004D4B8);
+
+INCLUDE_ASM("asm/nonmatchings/main/func_8004D190", func_8004D580);
+
+INCLUDE_ASM("asm/nonmatchings/main/func_8004D190", func_8004D5F0);
+
+INCLUDE_ASM("asm/nonmatchings/main/func_8004D190", func_8004D794);
+
+INCLUDE_ASM("asm/nonmatchings/main/func_8004D190", func_8004D7B8);
+
+INCLUDE_ASM("asm/nonmatchings/main/func_8004D190", set_flag_based_on_param);
