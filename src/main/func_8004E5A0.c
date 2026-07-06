ad225fdba07d8a8fb09d9d56669c428dd49d789f
Clyde Williams <hunterw297@gmail.com>
2026-07-05 22:10:56 -0500

    S190 freeze: func_8004E5A0.c 3-fn one-tu partial-bank, seed 13 classical

---
+++ src/main/func_8004E5A0.c
@@ -0,0 +1,7 @@
+#include "common.h"
+
+INCLUDE_ASM("asm/nonmatchings/main/func_8004E5A0", func_8004E5A0);
+
+INCLUDE_ASM("asm/nonmatchings/main/func_8004E5A0", func_8004E924);
+
+INCLUDE_ASM("asm/nonmatchings/main/func_8004E5A0", func_8004FDB4);
