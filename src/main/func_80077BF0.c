6f055ff9ded46a41e33cfec488d1f5296cce4b89
Clyde Williams <hunterw297@gmail.com>
2026-07-05 19:46:34 -0500

    S187: bank 4 small fns in func_80077BF0.c [4/7] (BF0/C0C/DEC/E6C, asm-first)

--- src/main/func_80077BF0.c
+++ src/main/func_80077BF0.c
@@ -1,14 +1,49 @@
 #include "common.h"
 
-INCLUDE_ASM("asm/nonmatchings/main/func_80077BF0", func_80077BF0);
+extern s32 D_80105148;
+extern u8 D_80105165;
+extern s32 D_800C4660;
+extern s32 D_800C4664;
+extern void* D_800FE338;
+extern u8 D_80105140[];
 
-INCLUDE_ASM("asm/nonmatchings/main/func_80077BF0", func_80077C0C);
+extern void heap3_free(void** payload_ptr);
+
+void func_80077BF0(void) {
+  D_80105148 = 150;
+  D_80105165 = 1;
+}
+
+void func_80077C0C(void) { D_80105165 = 0; }
 
 INCLUDE_ASM("asm/nonmatchings/main/func_80077BF0", func_80077C18);
 
-INCLUDE_ASM("asm/nonmatchings/main/func_80077BF0", func_80077DEC);
+void func_80077DEC(void) {
+  s32 i;
 
-INCLUDE_ASM("asm/nonmatchings/main/func_80077BF0", func_80077E6C);
+  if (D_800C4664 == 1) {
+    i = 0;
+    heap3_free((void**)&D_800FE338);
+    if (D_800C4660 != 0) {
+      do {
+        heap3_free((void**)&D_80105140[i * 0x2C]);
+        i++;
+      } while (i != D_800C4660);
+    }
+  }
+  D_800C4664 = 0;
+}
+
+s32 func_80077E6C(void) {
+  s32 v = D_800C4664;
+
+  if (v > 0) {
+    return 0;
+  }
+  v -= 1;
+  D_800C4664 = v;
+  return v < -2;
+}
 
 INCLUDE_ASM("asm/nonmatchings/main/func_80077BF0", func_80077E94);
 
