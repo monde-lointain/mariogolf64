9a7b965f86ab5c19f4514609eaeee892d72351de
Clyde Williams <hunterw297@gmail.com>
2026-07-05 19:41:38 -0500

    S187 freeze: func_80077BF0.c one-tu pack (7fn), seed 13 mixed

---
+++ src/main/func_80077BF0.c
@@ -0,0 +1,15 @@
+#include "common.h"
+
+INCLUDE_ASM("asm/nonmatchings/main/func_80077BF0", func_80077BF0);
+
+INCLUDE_ASM("asm/nonmatchings/main/func_80077BF0", func_80077C0C);
+
+INCLUDE_ASM("asm/nonmatchings/main/func_80077BF0", func_80077C18);
+
+INCLUDE_ASM("asm/nonmatchings/main/func_80077BF0", func_80077DEC);
+
+INCLUDE_ASM("asm/nonmatchings/main/func_80077BF0", func_80077E6C);
+
+INCLUDE_ASM("asm/nonmatchings/main/func_80077BF0", func_80077E94);
+
+INCLUDE_ASM("asm/nonmatchings/main/func_80077BF0", render_spark_effects);
