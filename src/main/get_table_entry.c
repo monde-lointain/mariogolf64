b10b8ab84df73f9b978d639249305371c0b1efab
Clyde Williams <hunterw297@gmail.com>
2026-07-06 07:49:58 -0500

    S191 freeze: get_table_entry.c 11-fn one-tu meter/ball/sound pack, seed 13 classical/mixed

---
+++ src/main/get_table_entry.c
@@ -0,0 +1,23 @@
+#include "common.h"
+
+INCLUDE_ASM("asm/nonmatchings/main/get_table_entry", get_table_entry);
+
+INCLUDE_ASM("asm/nonmatchings/main/get_table_entry", func_80032E88);
+
+INCLUDE_ASM("asm/nonmatchings/main/get_table_entry", play_sound_at_meter_ratio_gameplay);
+
+INCLUDE_ASM("asm/nonmatchings/main/get_table_entry", update_rotation_matrix);
+
+INCLUDE_ASM("asm/nonmatchings/main/get_table_entry", play_sound_at_meter_ratio);
+
+INCLUDE_ASM("asm/nonmatchings/main/get_table_entry", play_sound_for_meter_phase);
+
+INCLUDE_ASM("asm/nonmatchings/main/get_table_entry", read_meter_stick_normalized);
+
+INCLUDE_ASM("asm/nonmatchings/main/get_table_entry", update_ball_physics);
+
+INCLUDE_ASM("asm/nonmatchings/main/get_table_entry", func_80037E50);
+
+INCLUDE_ASM("asm/nonmatchings/main/get_table_entry", calc_stick_offset_with_noise);
+
+INCLUDE_ASM("asm/nonmatchings/main/get_table_entry", init_ball_for_shot);
