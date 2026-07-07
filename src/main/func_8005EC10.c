449f77fb2199f090c19e2d35684cfb3bef41771d
Clyde Williams <hunterw297@gmail.com>
2026-07-06 19:44:48 -0500

    Seed S195

---
+++ src/main/func_8005EC10.c
@@ -0,0 +1,9 @@
+#include "common.h"
+
+INCLUDE_ASM("asm/nonmatchings/main/func_8005EC10", func_8005EC10);
+
+INCLUDE_ASM("asm/nonmatchings/main/func_8005EC10", func_8005EC48);
+
+INCLUDE_ASM("asm/nonmatchings/main/func_8005EC10", func_8005ECC4);
+
+INCLUDE_ASM("asm/nonmatchings/main/func_8005EC10", audio_system_boot);
