453999fff9f970b59979b646478756ed33764d1f
Clyde Williams <hunterw297@gmail.com>
2026-07-07 08:05:21 -0500

    Bank func_800505A0

--- src/main/func_800505A0.c
+++ src/main/func_800505A0.c
@@ -1,6 +1,28 @@
 #include "common.h"
 
-INCLUDE_ASM("asm/nonmatchings/main/func_800505A0", func_800505A0);
+typedef struct {
+  /* 0x0 */ u32 start;
+  /* 0x4 */ u32 size;
+  /* 0x8 */ u32 pos;
+  /* 0xC */ u32 end;
+} RomLoadSlot; /* 0x10 */
+
+extern s32 D_8012D3A8;
+
+extern const char D_800CCB9C[];
+
+extern void osSyncPrintf(const char*, ...);
+extern void nuPiReadRom(u32 rom_addr, void* buf_ptr, u32 size);
+
+void func_800505A0(void* dst, u32 size, RomLoadSlot* slot) {
+  if (slot->end < slot->start + size) {
+    size = slot->end - slot->start;
+    osSyncPrintf(D_800CCB9C, D_8012D3A8);
+  }
+
+  nuPiReadRom(slot->start, dst, (size + 1) & ~1);
+  slot->start += size;
+}
 
 INCLUDE_ASM("asm/nonmatchings/main/func_800505A0", func_8005062C);
 
