1c7460dcea1e091a13bffa73a137361fa5880609
Clyde Williams <hunterw297@gmail.com>
2026-07-06 20:22:47 -0500

    match func_80050400

--- src/main/func_80050400.c
+++ src/main/func_80050400.c
@@ -1,6 +1,26 @@
 #include "common.h"
 
-INCLUDE_ASM("asm/nonmatchings/main/func_80050400", func_80050400);
+typedef struct {
+  /* 0x0 */ u32 start;
+  /* 0x4 */ u32 size;
+  /* 0x8 */ u32 pos;
+  /* 0xC */ u32 end;
+} RomLoadSlot; /* 0x10 */
+
+extern RomLoadSlot D_800DE8A0[2];
+
+void func_80050400(void) {
+  s32 i;
+  u8* slot;
+
+  i = 0;
+  slot = (u8*)D_800DE8A0;
+  do {
+    *(s32*)slot = 0;
+    i += 1;
+    slot += 0x10;
+  } while (i != 2);
+}
 
 INCLUDE_ASM("asm/nonmatchings/main/func_80050400", func_80050428);
 
