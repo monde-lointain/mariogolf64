1637310b3c6b2dfe099017f8158c9a65b864250e
Clyde Williams <hunterw297@gmail.com>
2026-07-06 20:10:07 -0500

    Revert "s196 seed"
    This reverts commit 26d7defae20cae5b2345afec4047d8081a3dddeb.

--- src/overlay_8/func_ovl8_801F55A0.c
+++
@@ -1,7 +0,0 @@
-#include "common.h"
-
-INCLUDE_ASM("asm/nonmatchings/overlay_8/func_ovl8_801F55A0", func_ovl8_801F55A0);
-
-INCLUDE_ASM("asm/nonmatchings/overlay_8/func_ovl8_801F55A0", func_ovl8_801F5604);
-
-INCLUDE_ASM("asm/nonmatchings/overlay_8/func_ovl8_801F55A0", func_ovl8_801F56E4);
