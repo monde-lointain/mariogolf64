93d9823bb2b793b713421ba244368518ae00242d
Clyde Williams <hunterw297@gmail.com>
2026-07-06 19:47:11 -0500

    Bank audio_system_boot

--- src/main/func_8005EC10.c
+++ src/main/func_8005EC10.c
@@ -2,6 +2,13 @@
 #include <nusys.h>
 #include <nualstl.h>
 
+extern musConfig D_800C3010;
+
+extern void bgm_alloc_song_buffer(s32 size);
+extern void func_8005F0BC(s32 size);
+
+void func_8005EC48(s16 arg0, s32 arg1);
+
 void func_8005EC10(NUAuPreNMIFunc func) {
   OSIntMask mask = osSetIntMask(OS_IM_NONE);
 
@@ -13,4 +20,13 @@ INCLUDE_ASM("asm/nonmatchings/main/func_8005EC10", func_8005EC48);
 
 INCLUDE_ASM("asm/nonmatchings/main/func_8005EC10", func_8005ECC4);
 
-INCLUDE_ASM("asm/nonmatchings/main/func_8005EC10", audio_system_boot);
+void audio_system_boot(musConfig* config) {
+  if (config == NULL) {
+    config = &D_800C3010;
+  }
+
+  nuAuStlMgrInit(config);
+  bgm_alloc_song_buffer(0xC000);
+  func_8005F0BC(0x4000);
+  func_8005EC10(func_8005EC48);
+}
