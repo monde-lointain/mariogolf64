04ce570eaaf48a08f1dbe57ba5ea816a33c2579e
Clyde Williams <hunterw297@gmail.com>
2026-07-06 20:19:26 -0500

    seed func_80050400 slice

---
+++ src/main/func_80050400.c
@@ -0,0 +1,13 @@
+#include "common.h"
+
+INCLUDE_ASM("asm/nonmatchings/main/func_80050400", func_80050400);
+
+INCLUDE_ASM("asm/nonmatchings/main/func_80050400", func_80050428);
+
+INCLUDE_ASM("asm/nonmatchings/main/func_80050400", func_800504E8);
+
+INCLUDE_ASM("asm/nonmatchings/main/func_80050400", func_80050504);
+
+INCLUDE_ASM("asm/nonmatchings/main/func_80050400", func_80050588);
+
+INCLUDE_ASM("asm/nonmatchings/main/func_80050400", func_80050598);
