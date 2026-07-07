26d7defae20cae5b2345afec4047d8081a3dddeb
Clyde Williams <hunterw297@gmail.com>
2026-07-06 19:58:51 -0500

    s196 seed

---
+++ src/overlay_8/func_ovl8_801F55A0.c
@@ -0,0 +1,7 @@
+#include "common.h"
+
+INCLUDE_ASM("asm/nonmatchings/overlay_8/func_ovl8_801F55A0", func_ovl8_801F55A0);
+
+INCLUDE_ASM("asm/nonmatchings/overlay_8/func_ovl8_801F55A0", func_ovl8_801F5604);
+
+INCLUDE_ASM("asm/nonmatchings/overlay_8/func_ovl8_801F55A0", func_ovl8_801F56E4);
