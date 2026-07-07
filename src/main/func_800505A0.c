f84c48baca73a3e729164d86a4d6c826f22d8674
Clyde Williams <hunterw297@gmail.com>
2026-07-07 08:00:35 -0500

    S198 seed

---
+++ src/main/func_800505A0.c
@@ -0,0 +1,7 @@
+#include "common.h"
+
+INCLUDE_ASM("asm/nonmatchings/main/func_800505A0", func_800505A0);
+
+INCLUDE_ASM("asm/nonmatchings/main/func_800505A0", func_8005062C);
+
+INCLUDE_ASM("asm/nonmatchings/main/func_800505A0", func_800506D4);
