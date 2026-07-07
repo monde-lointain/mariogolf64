2843f8a1125a7c5f563c136246e41e314d242e55
Clyde Williams <hunterw297@gmail.com>
2026-07-06 19:46:35 -0500

    Bank func_8005EC10

--- src/main/func_8005EC10.c
+++ src/main/func_8005EC10.c
@@ -1,6 +1,13 @@
 #include "common.h"
+#include <nusys.h>
+#include <nualstl.h>
 
-INCLUDE_ASM("asm/nonmatchings/main/func_8005EC10", func_8005EC10);
+void func_8005EC10(NUAuPreNMIFunc func) {
+  OSIntMask mask = osSetIntMask(OS_IM_NONE);
+
+  nuAuPreNMIFunc = func;
+  osSetIntMask(mask);
+}
 
 INCLUDE_ASM("asm/nonmatchings/main/func_8005EC10", func_8005EC48);
 
