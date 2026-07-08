03ab7018c7f8b928628115506dd59766eb3652ab
Clyde Williams <hunterw297@gmail.com>
2026-07-08 10:54:59 -0500

    Bank 8003E628 flags

--- src/main/func_8003E400.c
+++ src/main/func_8003E400.c
@@ -1,13 +1,17 @@
 #include "common.h"
 
+extern s8 D_800BA9A8;
+extern s8 D_800BA9A9;
+
 INCLUDE_ASM("asm/nonmatchings/main/func_8003E400", func_8003E400);
 
 INCLUDE_ASM("asm/nonmatchings/main/func_8003E400", func_8003E4B4);
 
-INCLUDE_ASM("asm/nonmatchings/main/func_8003E400", build_pin_or_cup_matrix_for_dad10);
+INCLUDE_ASM("asm/nonmatchings/main/func_8003E400",
+            build_pin_or_cup_matrix_for_dad10);
 
-INCLUDE_ASM("asm/nonmatchings/main/func_8003E400", func_8003E628);
+void func_8003E628(void) { D_800BA9A8 = 1; }
 
-INCLUDE_ASM("asm/nonmatchings/main/func_8003E400", func_8003E638);
+void func_8003E638(void) { D_800BA9A9 = 1; }
 
 INCLUDE_ASM("asm/nonmatchings/main/func_8003E400", func_8003E648);
