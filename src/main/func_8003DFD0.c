0c9a7c28f85b89003205c8afec4550b90b96900e
Clyde Williams <hunterw297@gmail.com>
2026-07-07 19:13:56 -0500

    freeze func_8003DFD0 seed

---
+++ src/main/func_8003DFD0.c
@@ -0,0 +1,7 @@
+#include "common.h"
+
+INCLUDE_ASM("asm/nonmatchings/main/func_8003DFD0", func_8003DFD0);
+
+INCLUDE_ASM("asm/nonmatchings/main/func_8003DFD0", func_8003E004);
+
+INCLUDE_ASM("asm/nonmatchings/main/func_8003DFD0", func_8003E314);
