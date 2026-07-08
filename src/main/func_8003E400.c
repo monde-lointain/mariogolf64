3a1643386a21cdd99dee4d41c20f76c684ac1343
Clyde Williams <hunterw297@gmail.com>
2026-07-08 10:51:07 -0500

    freeze func_8003E400 seed

---
+++ src/main/func_8003E400.c
@@ -0,0 +1,13 @@
+#include "common.h"
+
+INCLUDE_ASM("asm/nonmatchings/main/func_8003E400", func_8003E400);
+
+INCLUDE_ASM("asm/nonmatchings/main/func_8003E400", func_8003E4B4);
+
+INCLUDE_ASM("asm/nonmatchings/main/func_8003E400", build_pin_or_cup_matrix_for_dad10);
+
+INCLUDE_ASM("asm/nonmatchings/main/func_8003E400", func_8003E628);
+
+INCLUDE_ASM("asm/nonmatchings/main/func_8003E400", func_8003E638);
+
+INCLUDE_ASM("asm/nonmatchings/main/func_8003E400", func_8003E648);
