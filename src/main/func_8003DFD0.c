d3c024c9055f90eabe12dd96d68a82527388f420
Clyde Williams <hunterw297@gmail.com>
2026-07-07 19:17:39 -0500

    Bank func_8003DFD0

--- src/main/func_8003DFD0.c
+++ src/main/func_8003DFD0.c
@@ -1,6 +1,18 @@
 #include "common.h"
 
-INCLUDE_ASM("asm/nonmatchings/main/func_8003DFD0", func_8003DFD0);
+extern s32 calculate_hypotenuse_safe(s32, s32);
+
+s32 func_8003DFD0(s32 x, s32 z) {
+  s32 distance;
+
+  distance = calculate_hypotenuse_safe(x, z);
+  if ((u32)distance < 0x699U) {
+    distance = 0;
+  } else {
+    distance -= 0x699;
+  }
+  return distance;
+}
 
 INCLUDE_ASM("asm/nonmatchings/main/func_8003DFD0", func_8003E004);
 
