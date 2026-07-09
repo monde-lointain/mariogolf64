e4f8e56c379bdad8b4a0b76b92e0bdbc5612ac05
Clyde Williams <hunterw297@gmail.com>
2026-07-09 15:02:26 -0500

    Bank func_800779A8 (periodic cubic-Hermite spline lookup)
    S206: byte-exact via subagent decomp. Wrap x into [k0,k10), binary-search the
    11-knot interval, Hermite interpolate from arg2 values + arg3 tangents. Match
    levers: while-loops (not if/do-while), split t=x-base to pin x in fa0, non-
    negated bc1fl polarity, fully-inlined return for the local-alloc coloring tie.
    func_800772B0.c now 3/6 (3 FP-regalloc walls carried: 772C4/7775C/77AD4).

--- src/main/func_800772B0.c
+++ src/main/func_800772B0.c
@@ -12,7 +12,48 @@ INCLUDE_ASM("asm/nonmatchings/main/func_800772B0", func_800772C4);
 
 INCLUDE_ASM("asm/nonmatchings/main/func_800772B0", func_8007775C);
 
-INCLUDE_ASM("asm/nonmatchings/main/func_800772B0", func_800779A8);
+f32 func_800779A8(f32 x, f32* arg1, f32* arg2, f32* arg3) {
+  s32 mid;
+  s32 lo;
+  s32 hi;
+  f32 dx;
+  f32 q0;
+  f32 r0;
+  f32 r1;
+  f32 step;
+  f32 base;
+  f32 t;
+
+  step = arg1[10] - arg1[0];
+  while (arg1[10] < x) {
+    x -= step;
+  }
+  lo = 0;
+  while (x < arg1[0]) {
+    x += step;
+  }
+  lo = 0;
+  hi = 0xA;
+  do {
+    mid = (lo + hi) / 2;
+    if (arg1[mid] < x) {
+      lo = mid + 1;
+    } else {
+      hi = mid;
+    }
+  } while (lo < hi);
+  lo -= (lo > 0);
+  base = arg1[lo];
+  r1 = arg3[lo + 1];
+  t = x - base;
+  r0 = arg3[lo];
+  dx = arg1[lo + 1] - base;
+  q0 = arg2[lo];
+  return (((((((r1 - r0) * t) / dx) + (r0 * 3.0f)) * t) +
+           (((arg2[lo + 1] - q0) / dx) - (((r0 + r0) + r1) * dx))) *
+          t) +
+         q0;
+}
 
 INCLUDE_ASM("asm/nonmatchings/main/func_800772B0", func_80077AD4);
 
