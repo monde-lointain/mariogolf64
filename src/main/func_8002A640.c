f58c342665c970cbb87ae670836cc205852eacbc
Clyde Williams <hunterw297@gmail.com>
2026-07-24 09:40:01 -0500

    S269: flip func_8002A640 pack asm->c; bank func_80032658 (0x10) global-zero predicate

---
+++ src/main/func_8002A640.c
@@ -0,0 +1,54 @@
+#include "common.h"
+
+extern s32 D_800BB03C;
+
+INCLUDE_ASM("asm/nonmatchings/main/func_8002A640", func_8002A640);
+
+INCLUDE_ASM("asm/nonmatchings/main/func_8002A640", func_8002A90C);
+
+INCLUDE_ASM("asm/nonmatchings/main/func_8002A640", func_8002A944);
+
+INCLUDE_ASM("asm/nonmatchings/main/func_8002A640", func_8002A97C);
+
+INCLUDE_ASM("asm/nonmatchings/main/func_8002A640", func_8002A9C4);
+
+INCLUDE_ASM("asm/nonmatchings/main/func_8002A640", emit_sky_dome_dl);
+
+INCLUDE_ASM("asm/nonmatchings/main/func_8002A640", emit_sky_horizon_compositor_dl);
+
+INCLUDE_ASM("asm/nonmatchings/main/func_8002A640", init_rdp_and_draw_sky_background);
+
+void func_8002BD9C(void) {
+}
+
+INCLUDE_ASM("asm/nonmatchings/main/func_8002A640", setup_view_by_camera_mode);
+
+INCLUDE_ASM("asm/nonmatchings/main/func_8002A640", func_8002BE78);
+
+INCLUDE_ASM("asm/nonmatchings/main/func_8002A640", draw_ground_shadow_decals);
+
+INCLUDE_ASM("asm/nonmatchings/main/func_8002A640", func_8002CDA8);
+
+INCLUDE_ASM("asm/nonmatchings/main/func_8002A640", func_8002DAC0);
+
+INCLUDE_ASM("asm/nonmatchings/main/func_8002A640", render_frame);
+
+INCLUDE_ASM("asm/nonmatchings/main/func_8002A640", func_80031450);
+
+INCLUDE_ASM("asm/nonmatchings/main/func_8002A640", func_800318A8);
+
+INCLUDE_ASM("asm/nonmatchings/main/func_8002A640", func_80031AF4);
+
+INCLUDE_ASM("asm/nonmatchings/main/func_8002A640", func_80032520);
+
+s32 func_80032658(void) {
+    return D_800BB03C == 0;
+}
+
+INCLUDE_ASM("asm/nonmatchings/main/func_8002A640", func_80032668);
+
+INCLUDE_ASM("asm/nonmatchings/main/func_8002A640", func_800326C4);
+
+INCLUDE_ASM("asm/nonmatchings/main/func_8002A640", func_800326FC);
+
+INCLUDE_ASM("asm/nonmatchings/main/func_8002A640", func_80032720);
